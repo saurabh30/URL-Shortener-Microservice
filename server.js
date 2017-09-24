31b4f5b11f72640ec9a49b1c29a3b85053a52da8
System <none>
2017-09-24 10:39:20 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -61,12 +61,12 @@ app.get('/new/http://:id',function(req,res){
   var site=req.params.id;
   console.log(site);
   res.redirect('http://'+site);
-})
+});
 app.get('/new/https://:id',function(req,res){
   var site=req.params.id;
   console.log(site);
   res.redirect('https://'+site);
-})
+});
 //my code ends
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
